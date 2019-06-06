c2e87fac64935f8f4570f7fdd9d8b5e9b7de9ebf
Median88 <Katote88@gmail.com>
2019-06-06 21:08:42 +0300

    add promise

--- lesson-3/js/main.js
+++ lesson-3/js/main.js
@@ -1,19 +1,28 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-// let getRequest = (url, cb) => {
-//     let xhr = new XMLHttpRequest();
-//     // window.ActiveXObject ->  xhr = new ActiveXObject();
-//     xhr.open('GET', url, true);
-//     xhr.onreadystatechange = () => {
-//         if(xhr.readyState === 4){
-//             if(xhr.status !== 200){
-//                 console.log('error')
-//             } else {
-//                 cb(xhr.responseText)
+// let getRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         let xhr = new XMLHttpRequest();
+//         // window.ActiveXObject -> xhr = new ActiveXObject()
+//         xhr.open("GET", url, true);
+//         xhr.onreadystatechange = () => {
+//             if(xhr.readyState === 4){
+//                 if(xhr.status !== 200){
+//                     reject('Error');
+//                 } else {
+//                     resolve(xhr.responseText);
+//                 }
 //             }
-//         }
-//     }
+//         };
+//         xhr.send();
+//     })
 // };
+//
+// getRequest('tel.json').then(data => {
+//
+// })
+
+
 
 class List {
     constructor(url, container, list = list2){
